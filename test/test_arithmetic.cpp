62104f5dfb7cfb17241a9c3a1af1a26c1c48f1e6
Mario Falcao <mario@layer64.com>
2018-10-16 21:33:14 +0100

    improve unit tests - #1

--- test/test_arithmetic.cpp
+++ test/test_arithmetic.cpp
@@ -2,7 +2,145 @@
 #include "lib/catch.hpp"
 #include "../src/vm.h"
 
-#define _ALMOST_EQUAL(x, y) fabs(x - y) < 0.001
+#define _ALMOST_EQUAL(x, y) fabs(x - y) < 0.0001
+
+TEST_CASE("OP_INC")
+{
+    uint8_t program[] = {
+        OP_INC, R0,
+        OP_HALT};
+    VM vm(program);
+
+    SECTION("Zero")
+    {
+        vm.setRegister(R0, 0);
+        vm.run();
+        REQUIRE(vm.getRegister(R0) == 1);
+    }
+
+    SECTION("1123497651")
+    {
+        vm.reset();
+        vm.setRegister(R0, 1123497651);
+        vm.run();
+        REQUIRE(vm.getRegister(R0) == 1123497652);
+    }
+
+    SECTION("UINT32_MAX")
+    {
+        vm.reset();
+        vm.setRegister(R0, UINT32_MAX);
+        vm.run();
+        REQUIRE(vm.getRegister(R0) == 0);
+    }
+}
+
+TEST_CASE("OP_FINC")
+{
+    uint8_t program[] = {
+        OP_FINC, R0,
+        OP_HALT};
+    VM vm(program);
+
+    SECTION("Zero")
+    {
+        float val = 0.0f;
+        float expected = 1.0f;
+        vm.setRegister(R0, *((uint32_t*)&val));
+        vm.run();
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), expected));
+    }
+
+    SECTION("1123497.12")
+    {
+        float val = 1123497.12f;
+        float expected = 1123498.12f;
+        vm.setRegister(R0, *((uint32_t*)&val));
+        vm.run();
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), expected));
+    }
+
+    SECTION("-5")
+    {
+        float val = -5.0f;
+        float expected = -4.0f;
+        vm.setRegister(R0, *((uint32_t*)&val));
+        vm.run();
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), expected));
+    }
+}
+
+TEST_CASE("OP_DEC")
+{
+    uint8_t program[] = {
+        OP_DEC, R0,
+        OP_HALT};
+    VM vm(program);
+
+    SECTION("Zero")
+    {
+        vm.setRegister(R0, 0);
+        vm.run();
+        REQUIRE(vm.getRegister(R0) == UINT32_MAX);
+    }
+
+    SECTION("1123497651")
+    {
+        vm.reset();
+        vm.setRegister(R0, 1123497651);
+        vm.run();
+        REQUIRE(vm.getRegister(R0) == 1123497650);
+    }
+
+    SECTION("UINT32_MAX")
+    {
+        vm.reset();
+        vm.setRegister(R0, UINT32_MAX);
+        vm.run();
+        REQUIRE(vm.getRegister(R0) == (UINT32_MAX - 1));
+    }
+}
+
+TEST_CASE("OP_FDEC")
+{
+    uint8_t program[] = {
+        OP_FDEC, R0,
+        OP_HALT};
+    VM vm(program);
+
+    SECTION("Zero")
+    {
+        float val = 0.0f;
+        float expected = -1.0f;
+        vm.setRegister(R0, *((uint32_t*)&val));
+        vm.run();
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), expected));
+    }
+
+    SECTION("1123497.12")
+    {
+        float val = 1123497.12f;
+        float expected = 1123496.12f;
+        vm.setRegister(R0, *((uint32_t*)&val));
+        vm.run();
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), expected));
+    }
+
+    SECTION("-5")
+    {
+        float val = -5.0f;
+        float expected = -6.0f;
+        vm.setRegister(R0, *((uint32_t*)&val));
+        vm.run();
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), expected));
+    }
+}
 
 TEST_CASE("OP_ADD")
 {
@@ -51,7 +189,8 @@ TEST_CASE("OP_FADD")
         vm.setRegister(R1, *((uint32_t *)&val));
         vm.setRegister(R2, *((uint32_t *)&val));
         vm.run();
-        REQUIRE(vm.getRegister(R0) == *((uint32_t *)&val));
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), val));
     }
 
     SECTION("357.34 + 847.21")
@@ -63,7 +202,8 @@ TEST_CASE("OP_FADD")
         vm.setRegister(R1, *((uint32_t *)&val1));
         vm.setRegister(R2, *((uint32_t *)&val2));
         vm.run();
-        REQUIRE(vm.getRegister(R0) == *((uint32_t *)&expected));
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), expected));
     }
 
     SECTION("-3 + -5")
@@ -75,7 +215,8 @@ TEST_CASE("OP_FADD")
         vm.setRegister(R1, *((uint32_t *)&val1));
         vm.setRegister(R2, *((uint32_t *)&val2));
         vm.run();
-        REQUIRE(vm.getRegister(R0) == *((uint32_t *)&expected));
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), expected));
     }
 }
 
@@ -126,7 +267,8 @@ TEST_CASE("OP_FSUB")
         vm.setRegister(R1, *((uint32_t *)&val));
         vm.setRegister(R2, *((uint32_t *)&val));
         vm.run();
-        REQUIRE(vm.getRegister(R0) == *((uint32_t *)&val));
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), val));
     }
 
     SECTION("357.34 - 847.21")
@@ -151,7 +293,8 @@ TEST_CASE("OP_FSUB")
         vm.setRegister(R1, *((uint32_t *)&val1));
         vm.setRegister(R2, *((uint32_t *)&val2));
         vm.run();
-        REQUIRE(vm.getRegister(R0) == *((uint32_t *)&expected));
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), expected));
     }
 }
 
@@ -261,6 +404,110 @@ TEST_CASE("OP_FMUL")
         float val1 = -3.0f;
         float val2 = -5.0f;
         float expected = 15.0f;
+        vm.reset();
+        vm.setRegister(R1, *((uint32_t *)&val1));
+        vm.setRegister(R2, *((uint32_t *)&val2));
+        vm.run();
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), expected));
+    }
+}
+
+TEST_CASE("OP_DIV")
+{
+    uint8_t program[] = {
+        OP_DIV, R0, R1, R2,
+        OP_HALT};
+    VM vm(program);
+
+    // SECTION("1 / 0")
+    // {
+    // TODO: add code to test handling of division by zero
+    // }
+
+    SECTION("2 / 2")
+    {
+        vm.reset();
+        vm.setRegister(R1, 2);
+        vm.setRegister(R2, 2);
+        vm.run();
+        REQUIRE(vm.getRegister(R0) == 1);
+    }
+
+    SECTION("987513 / 347")
+    {
+        vm.reset();
+        vm.setRegister(R1, 987513);
+        vm.setRegister(R2, 347);
+        vm.run();
+        REQUIRE(vm.getRegister(R0) == 2845);
+    }
+}
+
+TEST_CASE("OP_IDIV")
+{
+    uint8_t program[] = {
+        OP_IDIV, R0, R1, R2,
+        OP_HALT};
+    VM vm(program);
+
+    // SECTION("1 / 0")
+    // {
+    // TODO: add code to test handling of division by zero
+    // }
+
+    SECTION("2 / 2")
+    {
+        vm.reset();
+        vm.setRegister(R1, 2);
+        vm.setRegister(R2, 2);
+        vm.run();
+        REQUIRE(vm.getRegister(R0) == 1);
+    }
+
+    SECTION("-50 / -3")
+    {
+        int32_t val1 = -50;
+        int32_t val2 = -3;
+        int32_t expected = 16;
+        vm.reset();
+        vm.setRegister(R1, *((uint32_t *)&val1));
+        vm.setRegister(R2, *((uint32_t *)&val2));
+        vm.run();
+        REQUIRE(vm.getRegister(R0) == *((uint32_t *)&expected));
+    }
+}
+
+TEST_CASE("OP_FDIV")
+{
+    uint8_t program[] = {
+        OP_FDIV, R0, R1, R2,
+        OP_HALT};
+    VM vm(program);
+
+    // SECTION("1 / 0")
+    // {
+    // TODO: add code to test handling of division by zero
+    // }
+
+    SECTION("847.21 / 357.34")
+    {
+        float val1 = 847.21f;
+        float val2 = 357.34f;
+        float expected = 2.370879275f;
+        vm.reset();
+        vm.setRegister(R1, *((uint32_t *)&val1));
+        vm.setRegister(R2, *((uint32_t *)&val2));
+        vm.run();
+        uint32_t actual = vm.getRegister(R0);
+        REQUIRE(_ALMOST_EQUAL(*((float *)&actual), expected));
+    }
+
+    SECTION("-50 / -3")
+    {
+        float val1 = -50.0f;
+        float val2 = -3.0f;
+        float expected = 16.666666667f;
         vm.reset();
         vm.setRegister(R1, *((uint32_t *)&val1));
         vm.setRegister(R2, *((uint32_t *)&val2));
